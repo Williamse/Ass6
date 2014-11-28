4546f984e9ce65530fe2a58a350801223b1f3b19
Williamse <ejw4720@g.rit.edu>
2014-11-28 15:57:12 -0500

    Initial

---
+++ Ass6/src/lightingParams.java
@@ -0,0 +1,41 @@
+//
+// lightingParams.java
+//
+// Simple class for setting up the viewing and projection transforms
+// for the Shading Assignment.
+//
+// Students are to complete this class.
+//
+
+import javax.media.opengl.*;
+import javax.media.opengl.fixedfunc.*; 
+
+public class lightingParams
+{
+    // Add any global class variables you need here.
+
+    /**
+     * constructor
+     */
+    public lightingParams()
+    {
+      
+    }
+    /**
+     * This functions sets up the lighting, material, and shading parameters
+     * for the Phong shader.
+     *
+     * You will need to write this function, and maintain all of the values
+     * needed to be sent to the vertex shader.
+     *
+     * @param program - The ID of an OpenGL (GLSL) shader program to which
+     * parameter values are to be sent
+     *
+     * @param gl2 - GL2 object on which all OpenGL calls are to be made
+     *
+     */
+    public void setUpPhong (int program, GL2 gl2)
+    {
+        // Add your code here.
+    }
+}
